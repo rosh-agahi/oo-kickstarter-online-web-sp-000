d7ab9e28e784a055f5e2719cbbddb8868afa0210
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-28 05:28:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,18 +14,3 @@ class Backer
   
 end
 
-class Project 
-  attr_reader :title 
-  attr_accessor :backer 
-  
-  def initialize(title)
-    @title = title 
-    @backers = []
-  end
-  
-  def add_backer(backer)
-    @backers << backer
-    backer.backed_projects << self
-  end
-  
-end
